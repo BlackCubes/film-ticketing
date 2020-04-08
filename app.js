c7deaadc84e2aaed0d7802c8492e7272821bd57d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 02:11:44 -0700

    Created a GET method for one show

--- app.js
+++ app.js
@@ -29,6 +29,25 @@ app.get('/api/v1/shows', (req, res) => {
   });
 });
 
+app.get('/api/v1/shows/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const show = shows.find(el => el.id === id);
+
+  if (!show) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      show
+    }
+  });
+});
+
 app.post('/api/v1/shows', (req, res) => {
   const newId = shows[shows.length - 1].id + 1;
   const newShow = Object.assign({ id: newId }, req.body);
