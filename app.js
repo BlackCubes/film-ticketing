612ad0e6a4c15b506a053939701d470f709add95
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 02:39:31 -0700

    Created PATCH method for a show

--- app.js
+++ app.js
@@ -68,6 +68,22 @@ app.post('/api/v1/shows', (req, res) => {
   );
 });
 
+app.patch('/api/v1/shows/:id', (req, res) => {
+  if (req.params.id * 1 > shows.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      shows: '<Updated show here...>'
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
