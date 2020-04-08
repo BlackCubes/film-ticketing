5e5438e3444d72da75611cabd23372b0b1ef5c82
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 13:31:07 -0700

    Removed comment

--- app.js
+++ app.js
@@ -5,16 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Kinetotickets' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
 const shows = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/shows-simple.json`)
 );
