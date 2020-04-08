3299d7f71a9cc4e4dc2b0dfcce8a934c15db9390
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 16:58:51 -0700

    Refactored the shows and users code

---
+++ controllers/showController.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+
+const shows = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/shows-simple.json`)
+);
+
+exports.getAllShows = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: shows.length,
+    data: {
+      shows
+    }
+  });
+};
+
+exports.getShow = (req, res) => {
+  const id = req.params.id * 1;
+  const show = shows.find(el => el.id === id);
+
+  if (!show) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      show
+    }
+  });
+};
+
+exports.createShow = (req, res) => {
+  const newId = shows[shows.length - 1].id + 1;
+  const newShow = Object.assign({ id: newId }, req.body);
+
+  shows.push(newShow);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/shows-simple.json`,
+    JSON.stringify(shows),
+    err => {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          shows: newShow
+        }
+      });
+    }
+  );
+};
+
+exports.updateShow = (req, res) => {
+  if (req.params.id * 1 > shows.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      shows: '<Updated show here...>'
+    }
+  });
+};
+
+exports.deleteShow = (req, res) => {
+  if (req.params.id * 1 > shows.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
---
+++ routes/showRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const showController = require('./../controllers/showController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(showController.getAllShows)
+  .post(showController.createShow);
+
+router
+  .route('/:id')
+  .get(showController.getShow)
+  .patch(showController.updateShow)
+  .delete(showController.deleteShow);
+
+module.exports = router;
