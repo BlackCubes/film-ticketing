b972dbc1fcf4a5b484aa88ca042f91ff98d0fa59
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-07 21:42:57 -0700

    Commented out some of the codes

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Kinetotickets' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Kinetotickets' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
 
 const port = 3000;
 app.listen(port, () => {
