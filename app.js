216846ca5caa098b1389ef473fded4d4d581f883
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 01:21:43 -0700

    Created POST method for shows

--- app.js
+++ app.js
@@ -29,6 +29,26 @@ app.get('/api/v1/shows', (req, res) => {
   });
 });
 
+app.post('/api/v1/shows', (req, res) => {
+  const newId = shows[shows.length - 1].id + 1;
+  const newShow = Object.assign({ id: newId }, req.body);
+
+  shows.push(newShow);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/shows-simple.json`,
+    JSON.stringify(shows),
+    err => {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          shows: newShow
+        }
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
