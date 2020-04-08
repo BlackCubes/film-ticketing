c944280f54cb4ac7df67409a38b9adf86a841f82
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 15:46:26 -0700

    Removed emoji

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`Hello from the middleware! 🤓`);
+  console.log('Hello from the middleware!');
   next();
 });
 
