f96e8ed575c1bce3142d6ac2873631b985c7bd95
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-04-08 17:13:14 -0700

    Exported app and imported it

--- app.js
+++ app.js
@@ -19,8 +19,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/shows', showRouter);
 app.use('/api/v1/users', userRouter);
 
-// Start Server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
