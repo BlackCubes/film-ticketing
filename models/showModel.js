04877a5afa88a3e4152de1d1e5df9f9ef512c396
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-13 22:48:35 -0700

    Updated the show schema

--- models/showModel.js
+++ models/showModel.js
@@ -4,15 +4,52 @@ const showSchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, 'A show must have a title!'],
-    unique: true
+    unique: true,
+    trim: true
+  },
+  year: {
+    type: Date,
+    required: [true, 'A show must have an original release year!']
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A show must have a duration!']
+  },
+  mpaaRating: {
+    type: String,
+    required: [true, 'A show must have a MPAA rating!']
+  },
+  releaseDate: Date,
+  overview: {
+    type: String,
+    required: [true, 'A show must have an overview!'],
+    trim: true
+  },
+  synopsis: {
+    type: String,
+    trim: true
+  },
+  poster: {
+    type: String,
+    required: [true, 'A show must have a poster!']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A show must have a price!']
   },
   ratingsAverage: {
     type: Number,
     default: 4.5
   },
-  price: {
+  ratingsQuantity: {
     type: Number,
-    required: [true, 'A show must have a price!']
+    default: 0
+  },
+  genres: [String],
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
   }
 });
 
