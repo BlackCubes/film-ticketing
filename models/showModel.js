2c8d5c5c9ff87f4851d9aec6aeb6322d293b115b
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-14 23:30:41 -0700

    Created virtual property for duration into hours

--- models/showModel.js
+++ models/showModel.js
@@ -1,57 +1,67 @@
 const mongoose = require('mongoose');
 
-const showSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'A show must have a title!'],
-    unique: true,
-    trim: true
+const showSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'A show must have a title!'],
+      unique: true,
+      trim: true
+    },
+    originalReleaseDate: {
+      type: [Date],
+      required: [true, 'A show must have an original release year!']
+    },
+    releaseDate: Date,
+    duration: {
+      type: Number,
+      required: [true, 'A show must have a duration!']
+    },
+    mpaaRating: {
+      type: String,
+      required: [true, 'A show must have a MPAA rating!']
+    },
+    overview: {
+      type: String,
+      required: [true, 'A show must have an overview!'],
+      trim: true
+    },
+    synopsis: {
+      type: String,
+      trim: true
+    },
+    poster: {
+      type: String,
+      required: [true, 'A show must have a poster!']
+    },
+    price: {
+      type: Number,
+      required: [true, 'A show must have a price!']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    genres: [String],
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    }
   },
-  originalReleaseDate: {
-    type: [Date],
-    required: [true, 'A show must have an original release year!']
-  },
-  releaseDate: Date,
-  duration: {
-    type: Number,
-    required: [true, 'A show must have a duration!']
-  },
-  mpaaRating: {
-    type: String,
-    required: [true, 'A show must have a MPAA rating!']
-  },
-  overview: {
-    type: String,
-    required: [true, 'A show must have an overview!'],
-    trim: true
-  },
-  synopsis: {
-    type: String,
-    trim: true
-  },
-  poster: {
-    type: String,
-    required: [true, 'A show must have a poster!']
-  },
-  price: {
-    type: Number,
-    required: [true, 'A show must have a price!']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  genres: [String],
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
+);
+
+showSchema.virtual('durationHours').get(function() {
+  return this.duration / 60;
 });
 
 const Show = mongoose.model('Show', showSchema);
