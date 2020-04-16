5c79e56ad128b065011056b5dc0b8f8c519a08f6
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-15 21:22:28 -0700

    Created error handling route and reformat the unhandled routes middleware

--- app.js
+++ app.js
@@ -23,11 +23,21 @@ app.use('/api/v1/shows', showRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Could not find ${req.originalUrl} on this server!`
+  const err = new Error(`Could not find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
-  next();
 });
 
 module.exports = app;
