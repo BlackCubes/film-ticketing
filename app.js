f6e7c7bec45900397013268ce74ffa67597fdc51
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-15 21:02:44 -0700

    Created unhandled routes

--- app.js
+++ app.js
@@ -13,10 +13,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Could not find ${req.originalUrl} on this server!`
+  });
   next();
 });
+
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware!');
+//   next();
+// });
 
 // Routes
 app.use('/api/v1/shows', showRouter);
