caa6fb940610c2be97e6ae04b1b1ac0f9dd94955
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-17 00:29:30 -0700

    Removed unneeded file

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const catchAsync = require('./../controllers/authController');
 
 const userSchema = new mongoose.Schema({
   name: {
