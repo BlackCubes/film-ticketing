d2baa640ed9e6599b24b377835f967c438564382
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-16 22:29:10 -0700

    Fixed security flaw with user's signing up to not be admins

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,15 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    birthdate: req.body.birthdate,
+    gender: req.body.gender
+  });
 
   res.status(201).json({
     status: 'success',
