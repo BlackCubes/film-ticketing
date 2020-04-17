34efcb4769691a6717d50408b1e6a150cc1b19a2
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-16 23:42:36 -0700

    Created instance method to check user correct password

--- models/userModel.js
+++ models/userModel.js
@@ -65,6 +65,14 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+// INSTANCE METHOD TO CHECK FOR CORRECT PASSWORD
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
