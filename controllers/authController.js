a979559617e609c96a3529e712b3132a4d51b062
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-17 12:00:39 -0700

    Testing the protect user/token route

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
     birthdate: req.body.birthdate,
     gender: req.body.gender
   });
