735ee32cf10321f34ae7f5f2352f8eb35fa652b7
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-18 11:07:07 -0700

    Created rate limiting middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  // Based on the type of app you have
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -21,7 +30,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
-// Routes
+// ROUTES
 app.use('/api/v1/shows', showRouter);
 app.use('/api/v1/users', userRouter);
 
