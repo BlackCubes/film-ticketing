f5a7c2586d266f45215a35b417467625faf1eb9d
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-18 11:09:08 -0700

    Commented the static middleware

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+//app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware!');
