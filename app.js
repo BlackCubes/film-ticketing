a07f1c777c3861dfe1b856f2009e15e487570edb
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-18 11:38:30 -0700

    Created data sanitization middlewares

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +32,13 @@ app.use('/api', limiter);
 
 // Body Parser -- Reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization
+// -- against NoSQL query injection
+app.use(mongoSanitize());
+
+// -- against XSS
+app.use(xss());
 
 // Static -- Displaying static files
 //app.use(express.static(`${__dirname}/public`));
