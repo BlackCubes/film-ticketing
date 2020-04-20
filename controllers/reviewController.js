f271fe342dea2031a1fd2413f12ba7c595b6de82
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-19 17:32:34 -0700

    Fixed syntax error

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -10,10 +10,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     data: {
       reviews
     }
-  })
+  });
 });
 
-exports.createReview = catchAsync(async (req, res, next) {
+exports.createReview = catchAsync(async (req, res, next) => {
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
@@ -21,5 +21,5 @@ exports.createReview = catchAsync(async (req, res, next) {
     data: {
       review: newReview
     }
-  })
+  });
 });
