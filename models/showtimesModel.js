e3582ad55c297676b437c5ddd806da6c15ffb935
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-20 10:42:42 -0700

    Created showtimes model schema

---
+++ models/showtimesModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const showtimesSchema = new mongoose.Schema(
+  {
+    startDateTime: {
+      type: Date,
+      required: [true, 'A show must have a start datetime!']
+    },
+    endDateTime: {
+      type: Date,
+      required: [true, 'A show must have an end datetime!']
+    },
+    shows: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Shows',
+      required: [true, 'A showtime must belong to a show!']
+    },
+    theaters: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Theaters',
+      required: [true, 'A showtime must belong to a theater!']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Showtimes = mongoose.model('Showtimes', showtimesSchema);
+
+module.exports = Showtimes;
