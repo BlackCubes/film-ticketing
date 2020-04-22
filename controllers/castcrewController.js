5af6b42d9eab010eea70606341d7587b15768b07
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-22 13:21:28 -0700

    Created get all cast/crew middleware

---
+++ controllers/castcrewController.js
@@ -0,0 +1,14 @@
+const CastCrew = require('./../models/castcrewModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllCastCrew = catchAsync(async (req, res, next) => {
+  const castcrews = await CastCrew.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: castcrews.length,
+    data: {
+      castcrews;
+    }
+  })
+})
