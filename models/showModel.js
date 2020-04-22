4a658a97bc6cb739f12955e7edf15c5c10fa3604
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-22 12:23:40 -0700

    Debugging the query find path middleware

--- models/showModel.js
+++ models/showModel.js
@@ -183,7 +183,7 @@ showSchema.pre('save', function(next) {
 // -- populate the cast/crew
 showSchema.pre(/^find/, function(next) {
   this.populate({
-    path: 'director',
+    path: 'CastCrew',
     select: 'name'
   });
 
