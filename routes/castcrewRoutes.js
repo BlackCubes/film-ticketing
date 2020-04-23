e4f08430e68b53df406d499703a9dd70a8bd17e1
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-22 23:14:52 -0700

    Added cast/crew patch with restriction middleware where only admin has access

--- routes/castcrewRoutes.js
+++ routes/castcrewRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const castcrewConroller = require('./../controllers/castcrewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router.route('/').get(castcrewConroller.getAllCastCrew);
 
-router.route('/:id').get(castcrewConroller.getCastCrew);
+router
+  .route('/:id')
+  .get(castcrewConroller.getCastCrew)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    castcrewConroller.updateCastCrew
+  );
 
 module.exports = router;
