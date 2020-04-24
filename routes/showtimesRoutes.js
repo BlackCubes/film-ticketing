52236f12fe816b971b70bf0096eb5638fb4d2f7e
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-23 20:10:48 -0700

    Added create showtime route only for event-owners to create

--- routes/showtimesRoutes.js
+++ routes/showtimesRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const showtimesController = require('./../controllers/showtimesController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(showtimesController.getAllShowtimes);
+router
+  .route('/')
+  .get(showtimesController.getAllShowtimes)
+  .post(
+    authController.protect,
+    authController.restrictTo('event-owner'),
+    showtimesController.createShowtime
+  );
 
 module.exports = router;
