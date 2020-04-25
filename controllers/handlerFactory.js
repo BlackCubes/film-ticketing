7ee7ec24e753f012e241077e95e13943cea6d5a2
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-25 13:11:53 -0700

    Refactored the get middleware with populate into handler factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -45,3 +45,23 @@ exports.createOne = Model =>
       }
     });
   });
+
+exports.getOne = (Model, ...popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('There is no document with that ID!', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
