fa4b5f817e7c160408f72f1da727774bbc7973fb
BlackCubes <gutierrezelias1991@gmail.com>
2020-04-28 13:38:18 -0700

    Removed the theater role

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   photo: String,
   role: {
     type: String,
-    enum: ['user', 'event-owner', 'theater', 'admin'],
+    enum: ['user', 'event-owner', 'admin'],
     default: 'user'
   },
   password: {
