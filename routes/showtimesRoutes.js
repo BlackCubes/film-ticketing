a581d870de2e08bfadda765799aec53fdcee16ad
BlackCubes <gutierrezelias1991@gmail.com>
2020-05-01 12:59:17 -0700

    Refactored to protect all other routes from leaking

--- routes/showtimesRoutes.js
+++ routes/showtimesRoutes.js
@@ -6,13 +6,14 @@ const router = express.Router({ mergeParams: true });
 
 router.route('/').get(showtimesController.getAllShowtimes);
 router.route('/:id').get(showtimesController.getShowtime);
-
 router.route('/daily-plan/:date').get(showtimesController.getDailyPlan);
+
+// PROTECT ALL OTHER ROUTES LEAKING
+router.use(authController.protect);
 
 router
   .route('/')
   .post(
-    authController.protect,
     authController.restrictTo('event-owner'),
     showtimesController.setShowTheaterIds,
     showtimesController.createShowtime
@@ -21,12 +22,10 @@ router
 router
   .route('/:id')
   .patch(
-    authController.protect,
     authController.restrictTo('event-owner'),
     showtimesController.updateShowtime
   )
   .delete(
-    authController.protect,
     authController.restrictTo('admin', 'event-owner'),
     showtimesController.deleteShowtime
   );
