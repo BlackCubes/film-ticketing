bdf8fc6728189261f42ef4559e9dfd9e536646c3
BlackCubes <gutierrezelias1991@gmail.com>
2020-05-01 13:44:24 -0700

    Refactored the code to protect and restrict certain routes

--- routes/theaterRoutes.js
+++ routes/theaterRoutes.js
@@ -8,27 +8,18 @@ const router = express.Router();
 router.use('/:theaterId/showtimes', showtimeRouter);
 router.use('/:theaterId/shows/:showId/showtimes', showtimeRouter);
 
-router
-  .route('/')
-  .get(theaterController.getAllTheaters)
-  .post(
-    authController.protect,
-    authController.restrictTo('admin'),
-    theaterController.createTheater
-  );
+router.route('/').get(theaterController.getAllTheaters);
+router.route('/:id').get(theaterController.getTheater);
+
+// PROTECT ALL OTHER ROUTES LEAKING AND RESTRICT ONLY TO ADMINS
+router.use(authController.protect);
+router.use(authController.restrictTo('admin'));
+
+router.route('/').post(theaterController.createTheater);
 
 router
   .route('/:id')
-  .get(theaterController.getTheater)
-  .patch(
-    authController.protect,
-    authController.restrictTo('admin'),
-    theaterController.updateTheater
-  )
-  .delete(
-    authController.protect,
-    authController.restrictTo('admin'),
-    theaterController.deleteTheater
-  );
+  .patch(theaterController.updateTheater)
+  .delete(theaterController.deleteTheater);
 
 module.exports = router;
