b8c3be0d23270d045b8f2e641c7ea73d27370ac2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-05-26 13:45:54 -0700

    Called a utils and show model, and used the utils

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,11 @@
-exports.getHome = (req, res) => {
+const catchAsync = require('./../utils/catchAsync');
+const Show = require('./../models/showModel');
+
+exports.getHome = catchAsync(async (req, res, next) => {
   res.status(200).render('home', {
     title: 'Rare Movie Tickets, Special Venues, Locations and Time'
   });
-};
+});
 
 exports.getShow = (req, res) => {
   res.status(200).render('show', {
