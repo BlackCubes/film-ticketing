19360044df282813b71282d86fb758c74b10866e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-05-28 14:02:16 -0700

    Created route for getting one review

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.get('/', viewsController.getHome);
 router.get('/shows', viewsController.getShows);
-router.get('/show-overview', viewsController.getShow);
+router.get('/show-overview/:slug', viewsController.getShow);
 
 module.exports = router;
