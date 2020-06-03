0a58edac1a536d3ad4782ff147602926bcbdfd8e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-03 00:44:08 -0700

    Created mapbox settings with access token

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,50 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYmxhY2tjdWJlcyIsImEiOiJjazc4MHQxeXAwZG5rM2ltOWc1NTV4dXY3In0.1HJOv2jH1Vg_GsXhiIWOkw';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/blackcubes/ck8f2ekqy0rzk1iljn5bg3fn6',
+  // interactive: false,
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.geo.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+    closeOnClick: false
+  })
+    .setLngLat(loc.geo.coordinates)
+    .setHTML(`<p>${loc.name}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.geo.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 50,
+    right: 50
+  }
+});
