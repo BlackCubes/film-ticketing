f0d762450ce1a7336b56634861e91c6f00e6f35e
BlackCubes <gutierrezelias1991@gmail.com>
2020-06-04 13:44:19 -0700

    Added maxZoom

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -7,7 +7,8 @@ mapboxgl.accessToken =
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/blackcubes/ck8f2ekqy0rzk1iljn5bg3fn6'
+  style: 'mapbox://styles/blackcubes/ck8f2ekqy0rzk1iljn5bg3fn6',
+  maxZoom: 10
   // interactive: false,
   // zoom: 10,
   // scrollZoom: false
