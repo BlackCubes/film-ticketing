b20b898e7207d52de1a0140afec2f56a3eafd0a1
BlackCubes <gutierrezelias1991@gmail.com>
2020-06-04 20:37:15 -0700

    Created a GET Theater route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,8 +7,12 @@ const router = express.Router();
 //   res.status(200).render('base');
 // });
 
+// SHOW ROUTES
 router.get('/', viewsController.getHome);
 router.get('/shows', viewsController.getShows);
 router.get('/show-overview/:slug', viewsController.getShow);
+
+// THEATER ROUTES
+router.get('/theater-overview/:slug', viewsController.getTheater);
 
 module.exports = router;
