36b593b987bb1ef5f8b79d2c94e1acad079c8368
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-06 10:52:04 -0700

    Created theaters route, and changed placement of comment

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,12 +7,14 @@ const router = express.Router();
 //   res.status(200).render('base');
 // });
 
-// SHOW ROUTES
 router.get('/', viewsController.getHome);
+
+// SHOW ROUTES
 router.get('/shows', viewsController.getShows);
 router.get('/show-overview/:slug', viewsController.getShow);
 
 // THEATER ROUTES
+router.get('/theaters', viewsController.getTheaters);
 router.get('/theater-overview/:slug', viewsController.getTheater);
 
 // CASTCREW ROUTES
