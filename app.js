abef0c5e10899986e83a0e937971ea4a780af453
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-06 18:01:05 -0700

    Used cookie-parser

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use('/api', limiter);
 
 // Body Parser -- reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data Sanitization
 // -- against NoSQL query injection
