58ee4fd5ecd6d487c204984782ad3c0f5db1fa19
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-06 17:58:20 -0700

    Impoted the cookie-parser

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
