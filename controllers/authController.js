f39bc4a2bd9dac748cc65845bbe51a4502ad11cd
BlackCubes <gutierrezelias1991@gmail.com>
2020-06-07 20:59:42 -0700

    Fixed syntax error

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,11 +69,11 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10*1000);
+    expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
   });
   res.status(200).json({ status: 'success' });
-}
+};
 
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
