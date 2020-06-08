e800c62203f2fae869c23dc447e0c7a676472024
BlackCubes <gutierrezelias1991@gmail.com>
2020-06-07 21:45:59 -0700

    Fixed syntax error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,7 +36,7 @@ const sendErrorDev = (err, req, res) => {
     });
   } else {
     // RENDERED WEBSITE
-    res.status(err.status).render({
+    res.status(err.statusCode).render({
       title: 'Uh Oh! Something is wrong!',
       msg: err.message
     });
