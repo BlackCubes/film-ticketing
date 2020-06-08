749ecddcfb885a5df0dab23ddfede9f943e469fb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-08 11:56:43 -0700

    Fixed bug by correctly including the right name of middleware

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -26,6 +26,6 @@ router.get('/castcrew-overview/:slug', viewsController.getCastCrew);
 // USER ROUTES
 router.get('/login', viewsController.getLoginForm);
 router.get('/register', viewsController.getRegisterForm);
-router.get('/forgotPassword', viewsController.getForgotPassword);
+router.get('/forgotPassword', viewsController.getForgotPasswordForm);
 
 module.exports = router;
