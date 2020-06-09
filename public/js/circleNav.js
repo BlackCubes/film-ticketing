235a381444e037c443b9a5377c21e88d2625d77d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-08 22:50:59 -0700

    Disabled eslint

--- public/js/circleNav.js
+++ public/js/circleNav.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export const circleNav = () => {
   var circleNavBtn = document.getElementById('circleNavBtn');
   var circleNavWrapper = document.getElementById('circleNavWrapper');
