60d1034024abf1ecfdeba5f090399c19af675d90
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-08 22:49:34 -0700

    Added parts of the circleNav JS fiel

---
+++ public/js/circleNav.js
@@ -0,0 +1,5 @@
+export const circleNav = () => {
+  var circleNavBtn = document.getElementById('circleNavBtn');
+  var circleNavWrapper = document.getElementById('circleNavWrapper');
+  var circleNavOverlay = document.getElementById('circleNavOverlay');
+};
