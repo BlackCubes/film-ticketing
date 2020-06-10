c21953e9c5a70d6f316b9a1365537199e5b74d5b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-09 20:07:24 -0700

    Commented out the circleNav file and copy-pasted its code for testing

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import '@babel/polyfill';
 require('./modernizr');
+import gsap from 'gsap';
 import { displayMap } from './mapbox';
 import { login, logout, register, forgotPassword } from './login';
-import './circleNav';
+// import './circleNav';
 
 // MODERNIZR TEST
 // if (Modernizr.csstransforms)
@@ -77,3 +78,56 @@ if (forgotPassForm) {
     forgotPassword(email);
   });
 }
+
+var svg = document.getElementById('menu'),
+  items = svg.querySelectorAll('.item'),
+  trigger = document.getElementById('trigger'),
+  label = trigger.querySelectorAll('#label')[0],
+  open = false;
+
+gsap.set(items, { scale: 0, visibility: 'visible' });
+svg.style.pointerEvents = 'none';
+
+trigger.addEventListener('click', toggleMenu, false);
+
+const toggleMenu = e => {
+  if (!e) var e = window.event;
+  e.stopPropagation();
+  open = !open;
+
+  if (open) {
+    gsap.to(items, {
+      duration: 0.7,
+      scale: 1,
+      ease: Elastic.easeOut,
+      stagger: 0.05
+    });
+    label.innerHTML = '-';
+    svg.style.pointerEvents = 'auto';
+  } else {
+    gsap.to(items, {
+      duration: 0.3,
+      scale: 0,
+      ease: Back.easeIn,
+      stagger: 0.05
+    });
+    label.innerHTML = '+';
+    svg.style.pointerEvents = 'none';
+  }
+};
+
+svg.onclick = function(e) {
+  e.stopPropagation();
+};
+
+document.onclick = function() {
+  open = false;
+  gsap.to(items, {
+    duration: 0.3,
+    scale: 0,
+    ease: Back.easeIn,
+    stagger: 0.05
+  });
+  label.innerHTML = '+';
+  svg.style.pointerEvents = 'none';
+};
