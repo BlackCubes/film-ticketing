0a82b85da112ff7f2e3f65336b68245442784259
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-09 22:39:48 -0700

    Commented out open navigation code

--- public/js/index.js
+++ public/js/index.js
@@ -79,58 +79,58 @@ if (forgotPassForm) {
   });
 }
 
-var svg = document.getElementById('menu'),
-  items = svg.querySelectorAll('.item'),
-  trigger = document.getElementById('trigger'),
-  label = trigger.querySelectorAll('#label')[0],
-  open = false;
+// var svg = document.getElementById('menu'),
+//   items = svg.querySelectorAll('.item'),
+//   trigger = document.getElementById('trigger'),
+//   label = trigger.querySelectorAll('#label')[0],
+//   open = false;
 
-gsap.set(items, { scale: 0, visibility: 'visible' });
-svg.style.pointerEvents = 'none';
+// gsap.set(items, { scale: 0, visibility: 'visible' });
+// svg.style.pointerEvents = 'none';
 
-trigger.addEventListener('click', toggleMenu, false);
+// trigger.addEventListener('click', toggleMenu, false);
 
-function toggleMenu(e) {
-  if (!e) var e = window.event;
-  e.stopPropagation();
-  open = !open;
+// function toggleMenu(e) {
+//   if (!e) var e = window.event;
+//   e.stopPropagation();
+//   open = !open;
 
-  if (open) {
-    gsap.to(items, {
-      duration: 0.7,
-      scale: 1,
-      ease: 'elastic.out(0.05)'
-      // ease: Elastic.easeOut,
-      // stagger: 0.05
-    });
-    label.innerHTML = '-';
-    svg.style.pointerEvents = 'auto';
-  } else {
-    gsap.to(items, {
-      duration: 0.3,
-      scale: 0,
-      ease: 'back.in(0.05)'
-      // ease: Back.easeIn,
-      // stagger: 0.05
-    });
-    label.innerHTML = '+';
-    svg.style.pointerEvents = 'none';
-  }
-}
+//   if (open) {
+//     gsap.to(items, {
+//       duration: 0.7,
+//       scale: 1,
+//       ease: 'elastic.out(0.05)'
+//       // ease: Elastic.easeOut,
+//       // stagger: 0.05
+//     });
+//     label.innerHTML = '-';
+//     svg.style.pointerEvents = 'auto';
+//   } else {
+//     gsap.to(items, {
+//       duration: 0.3,
+//       scale: 0,
+//       ease: 'back.in(0.05)'
+//       // ease: Back.easeIn,
+//       // stagger: 0.05
+//     });
+//     label.innerHTML = '+';
+//     svg.style.pointerEvents = 'none';
+//   }
+// }
 
-svg.onclick = function(e) {
-  e.stopPropagation();
-};
+// svg.onclick = function(e) {
+//   e.stopPropagation();
+// };
 
-document.onclick = function() {
-  open = false;
-  gsap.to(items, {
-    duration: 0.3,
-    scale: 0,
-    ease: 'back.in(0.05)'
-    // ease: Back.easeIn,
-    // stagger: 0.05
-  });
-  label.innerHTML = '+';
-  svg.style.pointerEvents = 'none';
-};
+// document.onclick = function() {
+//   open = false;
+//   gsap.to(items, {
+//     duration: 0.3,
+//     scale: 0,
+//     ease: 'back.in(0.05)'
+//     // ease: Back.easeIn,
+//     // stagger: 0.05
+//   });
+//   label.innerHTML = '+';
+//   svg.style.pointerEvents = 'none';
+// };
