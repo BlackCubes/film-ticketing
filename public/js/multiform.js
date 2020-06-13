7083a81e712571afe8bed5e6307bbdffe49e7a0a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-12 18:33:06 -0700

    Finished half of the form

--- public/js/multiform.js
+++ public/js/multiform.js
@@ -1,13 +1,7 @@
 /* eslint-disable */
 import { formValidator } from './errorController';
 
-export default class Form {
-  constructor(...inputs) {
-    this.inputs = inputs;
-  }
-}
-
-export class MultiForm {
+class MultiForm {
   constructor(button, fieldset, percentage) {
     this.button = button;
     this.fieldset = fieldset;
@@ -22,5 +16,30 @@ export class MultiForm {
 
   formSlide() {
     return (this.fieldset.style.marginLeft = this.percentage);
+  }
+}
+
+export default class Form extends MultiForm {
+  constructor(button, fieldset, percentage, input, type) {
+    super();
+    this.input = input;
+    this.type = type;
+  }
+
+  formGetElements(DOM) {
+    this.input = document.getElementById(`${DOM}`);
+    return this.input;
+  }
+
+  formCheck() {
+    formValidator(this.formGetElements, this.type);
+  }
+
+  formMulti() {
+    if (this.button) {
+      this.button.addEventListener('click', () => {
+        this.formCheck();
+      });
+    }
   }
 }
