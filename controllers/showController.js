98a97b801acc5a55dbe6ca5a912fa7b505338aae
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-14 17:05:01 -0700

    Added test multerStorage for API

--- controllers/showController.js
+++ controllers/showController.js
@@ -5,7 +5,18 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.memoryStorage();
+// MULTER
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+// const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
