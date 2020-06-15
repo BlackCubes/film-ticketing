97a2d9f78df8101a89b221596d4e4724e8846a3f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-15 11:21:17 -0700

    Debugging on the first fieldset

--- public/js/multiform.js
+++ public/js/multiform.js
@@ -28,7 +28,7 @@ export class MultiForm {
   }
 
   manipulateFirstFieldset() {
-    this.fieldset.style.width = '-20%';
+    document.querySelector('.form__fieldset--fifth').style.width = '-20%';
   }
 
   // For forward, margin-left values start at -25% and are every -25%: -25%, -50%, -75%, etc.
