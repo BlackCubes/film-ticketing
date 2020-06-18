4044344440b1a05b20e7ff78d88661442115e61e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-18 11:56:53 -0700

    Commented out old code, and created new Email class for users signing up

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,6 +48,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     gender: req.body.gender
   });
 
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 });
 
@@ -176,11 +179,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to ${resetURL}\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 mins)',
-      message
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 mins)',
+    //   message
+    // });
 
     res.status(200).json({
       status: 'success',
