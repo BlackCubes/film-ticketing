23d09c1a863922e5ff5a8ba3bcc2ff9f80972edd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-25 11:15:00 -0700

    Created a get router for ticket checkout session

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -4,4 +4,10 @@ const ticketController = require('./../controllers/ticketController');
 
 const router = express.Router();
 
+router.get(
+  '/checkout-session/:showId',
+  authController.protect,
+  ticketController.getCheckoutSession
+);
+
 module.exports = router;
