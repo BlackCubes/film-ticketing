e347e61d76764760cfb53e7e9de80d59e637c8ad
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-25 11:14:04 -0700

    Created get checkout session for tickets middleware

---
+++ controllers/ticketController.js
@@ -0,0 +1,34 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Show = require('./../models/showModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const show = await Show.findById(req.params.showId);
+
+  const session = await stripe.checkout.session.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/show-overview/${
+      show.slug
+    }`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.showId,
+    line_items: [
+      {
+        name: `${show.title} Show`,
+        description: show.overview,
+        // images: [],
+        amount: show.price * 100,
+        currency: 'usd',
+        quantity: 1
+      }
+    ]
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session
+  });
+});
