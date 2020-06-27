c4e1a4f4f1fae1065fbca9b3989f052d2ce806fb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-27 09:54:16 -0700

    Ran parcel

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -15831,7 +15831,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59619" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55629" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
