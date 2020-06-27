1b79a55616876addecad98a0cf496ed33ef4e160
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-26 19:36:28 -0700

    Created a update review api middleware for user

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,44 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.updateMyReview = catchAsync(async (req, res, next) => {
+  const filteredBody = filterObj(req.body, 'review', 'rating');
+
+  const updatedReview = await Review.findOneAndUpdate(
+    { show: req.params.showId, user: req.user.id },
+    filteredBody,
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review: updatedReview
+    }
+  });
+});
+
+exports.setShowUserIds = (req, res, next) => {
+  if (!req.body.show) req.body.show = req.params.showId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
 
 // exports.getAllReviews = catchAsync(async (req, res, next) => {
 //   let filter = {};
@@ -18,14 +55,3 @@ exports.getAllReviews = factory.getAll(Review);
 //     }
 //   });
 // });
-
-exports.setShowUserIds = (req, res, next) => {
-  if (!req.body.show) req.body.show = req.params.showId;
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
-};
-
-exports.getReview = factory.getOne(Review);
-exports.createReview = factory.createOne(Review);
-exports.updateReview = factory.updateOne(Review);
-exports.deleteReview = factory.deleteOne(Review);
