b0030663726a6452f9f03eb7a8723a39610d6012
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-26 20:59:35 -0700

    Added a delete review function for the front-end with axios

---
+++ public/js/deleteSettings.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const deleteReview = async (data, reviewId) => {
+  try {
+    const url = `http://127.0.0.1:3000/api/v1/reviews/deleteMyReview/${reviewId}`;
+
+    const res = await axios({
+      method: 'DELETE',
+      url,
+      data
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Review deleted!');
+      window.setTimeout(() => {
+        location.assign('/myReviews');
+      }, 1000);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
