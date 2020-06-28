c4dd9ba42bc093060f21b977989967ae9e7ac088
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-27 21:08:17 -0700

    Added the poster into req.body

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -40,6 +40,8 @@ exports.updateOne = Model =>
 
 exports.createOne = Model =>
   catchAsync(async (req, res, next) => {
+    if (req.file) req.body.poster = { urlLarge: req.file.filename };
+
     const doc = await Model.create(req.body);
 
     res.status(201).json({
