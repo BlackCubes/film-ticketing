76a3f912e3962b61ea27fb50b319ee653aea6a95
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-06-28 21:53:00 -0700

    Added verify password auth middleware into route

--- routes/theaterRoutes.js
+++ routes/theaterRoutes.js
@@ -35,6 +35,6 @@ router
 router
   .route('/:id')
   .patch(theaterController.updateTheater)
-  .delete(theaterController.deleteTheater);
+  .delete(authController.verifyPassword, theaterController.deleteTheater);
 
 module.exports = router;
