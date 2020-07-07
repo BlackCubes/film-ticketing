fea5e934147f77f186445ebe534f6d1173f2ed82
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-07-06 21:38:00 -0700

    Debugging

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -39,12 +39,11 @@ exports.getShow = catchAsync(async (req, res, next) => {
     return next(new AppError('There is no show with that name!', 404));
   }
 
-  const ticket = await Ticket.findOne({ show: show.id, user: req.user.id });
+  // const ticket = await Ticket.findOne({ show: show.id, user: req.user.id });
 
   res.status(200).render('show-overview', {
     title: show.title,
-    show,
-    ticket
+    show
   });
 });
 
