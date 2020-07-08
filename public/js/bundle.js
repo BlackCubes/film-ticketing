96006e838635fcd7c06a7f93a90a3ff8b3136b39
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-07-07 20:05:53 -0700

    Created a map for homepage

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12312,7 +12312,7 @@ exports.default = exports.gsap = gsapWithCSS;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.displayMap = void 0;
+exports.displayHomeMap = exports.displayMap = void 0;
 
 /* eslint-disable */
 var displayMap = function displayMap(locations) {
@@ -12355,6 +12355,96 @@ var displayMap = function displayMap(locations) {
 };
 
 exports.displayMap = displayMap;
+
+var displayHomeMap = function displayHomeMap() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYmxhY2tjdWJlcyIsImEiOiJjazc4MHQxeXAwZG5rM2ltOWc1NTV4dXY3In0.1HJOv2jH1Vg_GsXhiIWOkw';
+  var geoJson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.449444, 34.062678]
+      },
+      properties: {
+        description: 'Regency Village Theater'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.2505173, 34.050838]
+      },
+      properties: {
+        description: 'Million Dollar Theater'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-118.3911507, 33.903495]
+      },
+      properties: {
+        description: 'ArcLight Cinemas - Beach Cities'
+      }
+    }]
+  };
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/blackcubes/ck8f2ekqy0rzk1iljn5bg3fn6',
+    scrollZoom: false
+  });
+  var bounds = new mapboxgl.LngLatBounds();
+  geoJson.features.forEach(function (marker) {
+    var el = document.createElement('div');
+    el.className = 'marker';
+    new mapboxgl.Marker({
+      element: el,
+      anchor: bottom
+    }).setLngLat(marker.geometry.coordinates).addTo(map);
+    new mapboxgl.Popup({
+      offset: 30,
+      closeOnClick: false
+    }).setLngLat(marker.geometry.coordinates).setHTML('<p class="map-paragraph">' + marker.properties.description + '</p>').addTo(map);
+    bounds.extend(marker.geometry.coordinates);
+  });
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 50,
+      right: 50
+    }
+  });
+  map.on('load', function () {
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geoJson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: [[-118.449444, 34.062678], [-118.2505173, 34.050838], [-118.3911507, 33.903495]]
+          }
+        }
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#FF0000',
+        'line-opacity': 0.6,
+        'line-width': 3
+      }
+    });
+  });
+};
+
+exports.displayHomeMap = displayHomeMap;
 },{}],"../../node_modules/axios/lib/helpers/bind.js":[function(require,module,exports) {
 'use strict';
 
