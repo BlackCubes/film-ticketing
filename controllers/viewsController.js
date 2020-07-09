bd2c8bf93d70612f85a6212e2ffab7d0297db351
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-07-09 11:18:24 -0700

    Fixed pug syntax error

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -217,7 +217,7 @@ exports.getAdminUsers = catchAsync(async (req, res, next) => {
 exports.getAdminUser = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
-  res.status(200).render('account', {
+  res.status(200).render('account/account', {
     title: `Admin - Users: ${user.name}`,
     user
   });
