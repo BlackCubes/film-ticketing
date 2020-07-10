9e55b322794e10f74b68ee47fb53219479038e01
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-07-10 14:40:29 -0700

    Added auth restriction for admin and users

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get(
   '/checkout-session/:showId/:theaterId/:showtimeId',
   authController.protect,
+  authController.restrictTo('admin', 'user'),
   ticketController.getCheckoutSession
 );
 
