be40db93ccbed3373e2307d3e24db18a5e5f926b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-06 12:15:55 -0700

    Refactored

--- public/js/formController.js
+++ public/js/formController.js
@@ -51,53 +51,6 @@ export const checkFormSubmit = (...inputs) => {
       }
     }
   });
-
-  // const eVal = e.value.trim();
-
-  // if (e.name === 'email') {
-  //   if (eVal === '') {
-  //     formError(e, 'Please provide an email');
-  //     // formStatus -= 1;
-  //   } else if (!regexForm(e)) {
-  //     formError(e, 'Please provide a valid email address');
-  //     // formStatus -= 1;
-  //   } else {
-  //     formSuccess(e, 'Woohoo!');
-  //     formStatus += 1;
-  //     console.log('Email part: ', formStatus);
-  //   }
-  // }
-
-  // if (e.name === 'password') {
-  //   if (eVal === '') {
-  //     formError(e, 'Please provide a password');
-  //     // formStatus -= 1;
-  //   } else if (!regexForm(e)) {
-  //     formError(
-  //       e,
-  //       'Please use at least one number, one special character, and one capital letter between 8 to 60 characters'
-  //     );
-  //     // formStatus -= 1;
-  //   } else {
-  //     formSuccess(e, 'Woohoo!');
-  //     formStatus += 1;
-  //     console.log('Password part: ', formStatus);
-  //   }
-  // }
-
-  // if (e2 !== undefined && e2.name === 'password-confirm') {
-  //   if (e2.value === '') {
-  //     formError(e2, 'Please enter your password to confirm');
-  //     // formStatus -= 1;
-  //   } else if (e2.value.trim() !== eVal && e2.value !== '') {
-  //     formError(e2, 'Please make sure your passwords match');
-  //     // formStatus -= 1;
-  //   } else {
-  //     formSuccess(e2, 'Woohoo!');
-  //     formStatus += 1;
-  //     console.log('Password confirm part: ', formStatus);
-  //   }
-  // }
 };
 
 function regexForm(e) {
