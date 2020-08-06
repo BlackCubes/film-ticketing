6f8525047cf4b361ac34a83649133188addd2a97
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-05 17:48:24 -0700

    Created a check form function

---
+++ public/js/formController.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import { formError, formSuccess } from './errorController';
+
+export const checkForm = (email, password) => {
+  const emailVal = email.value.trim();
+  const passwordVal = password.value.trim();
+
+  if (emailVal === '') {
+    formError(email, 'Please provide an email');
+  } else {
+    formSuccess(email);
+  }
+
+  if (passwordVal === '') {
+    formError(password, 'Please provide a password');
+  } else {
+    formSuccess(password);
+  }
+};
