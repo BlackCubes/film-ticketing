4d865dee6d99ee86a8f32d412ed14412ac100037
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-09 19:41:06 -0700

    Created a function to dynamically change the rating stars

---
+++ public/js/ratingStars.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+export const ratingStars = () => {
+  var stars = document.querySelectorAll('.star');
+
+  stars.forEach(function(star) {
+    star.addEventListener('click', setRating);
+  });
+
+  var rating = parseInt(
+    document.querySelector('.star').getAttribute('data-rating')
+  );
+  var target = stars[rating - 1];
+  target.dispatchEvent(new MouseEvent('click'));
+};
+
+function setRating(e) {
+  var span = e.currentTarget,
+    stars = document.querySelectorAll('.star'),
+    match = false,
+    num = 0;
+
+  stars.forEach(function(star, index) {
+    if (match) {
+      star.classList.remove('rated');
+    } else {
+      star.classList.add('rated');
+    }
+
+    if (star === span) {
+      match = true;
+      num = index + 1;
+    }
+  });
+
+  document.querySelector('.stars').setAttribute('data-rating', num);
+}
