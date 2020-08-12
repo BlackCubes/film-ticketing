a3143a929b2220a5f4f8f6a6f74545a81b24d06d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-12 12:06:12 -0700

    Debugging

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -47,20 +47,20 @@ router.get(
 // USER ROUTES
 router.get(
   '/login',
-  // authController.isLoggedIn,
-  authController.restrictUsers('user', 'admin', 'event-owner'),
+  authController.isLoggedIn,
+  // authController.restrictUsers('user', 'admin', 'event-owner'),
   viewsController.getLoginForm
 );
 router.get(
   '/register',
-  // authController.isLoggedIn,
-  authController.restrictUsers('user', 'admin', 'event-owner'),
+  authController.isLoggedIn,
+  // authController.restrictUsers('user', 'admin', 'event-owner'),
   viewsController.getRegisterForm
 );
 router.get(
   '/forgotPassword',
-  // authController.isLoggedIn,
-  authController.restrictUsers('user', 'admin', 'event-owner'),
+  authController.isLoggedIn,
+  // authController.restrictUsers('user', 'admin', 'event-owner'),
   viewsController.getForgotPasswordForm
 );
 router.get('/me', authController.protect, viewsController.getAccount);
