4a4a741c95dee62109610f52f4b3734995b14fae
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-23 11:25:52 -0700

    Implemented multer with memory stream

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,6 +1,27 @@
 var cloudinary = require('cloudinary').v2;
+const multer = require('multer');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError('This is not an image! Please upload only images!', 400),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+upload.single('poster');
 
 exports.uploadPhoto = (req, res, next) => {
   if (!req.file) return next();
