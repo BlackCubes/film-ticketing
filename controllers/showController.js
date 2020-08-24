89e8a613290b0849e3bbba8fdb9a32a6865ad2cd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-23 20:48:21 -0700

    Debugging

--- controllers/showController.js
+++ controllers/showController.js
@@ -227,7 +227,7 @@ exports.updateMyShow = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      show: updatedShow
+      show: filteredBody
     }
   });
 });
