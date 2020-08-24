2f10a67832fdfb7b9ffdcde1e65bd2219d25e6d4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-23 17:43:50 -0700

    Debugging the error

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -26,61 +26,61 @@ const upload = multer({
 
 exports.bufferPhoto = key => upload.single(`${key}`);
 
-exports.uploadPhoto = catchAsync(async (req, res, next) => {
+exports.uploadPhoto = async (req, res, next) => {
   if (!req.file) return next();
   let cloudinaryId = '';
   let cloudinaryUrl = '';
   const testing = 'hello';
 
-  const cloudinaryUpload = stream => {
-    const cloudinaryStream = cloudinary.uploader.upload_stream(
-      {
-        upload_preset: 'kinetotickets-shows'
-      },
-      function(error, result) {
-        if (result) {
-          cloudinaryId = result.public_id;
-          cloudinaryUrl = result.secure_url;
-        } else {
-          return next(
-            new AppError(
-              'There is a problem uploading your image! Please contact the system administrator',
-              500
-            )
-          );
-        }
-      }
-    );
+  // const cloudinaryUpload = stream => {
+  //   const cloudinaryStream = cloudinary.uploader.upload_stream(
+  //     {
+  //       upload_preset: 'kinetotickets-shows'
+  //     },
+  //     function(error, result) {
+  //       if (result) {
+  //         cloudinaryId = result.public_id;
+  //         cloudinaryUrl = result.secure_url;
+  //       } else {
+  //         return next(
+  //           new AppError(
+  //             'There is a problem uploading your image! Please contact the system administrator',
+  //             500
+  //           )
+  //         );
+  //       }
+  //     }
+  //   );
 
-    streamifier.createReadStream(stream).pipe(cloudinaryStream);
-  };
-
-  // const cloudinaryUpload = async stream => {
-  //   try {
-  //     await new Promise((resolve, reject) => {
-  //       const cloudinaryStream = cloudinary.uploader.upload_stream(
-  //         {
-  //           upload_preset: 'kinetotickets-shows'
-  //         },
-  //         function(error, result) {
-  //           if (result) {
-  //             cloudinaryId = result.public_id;
-  //             cloudinaryUrl = result.secure_url;
-  //             resolve(cloudinaryId);
-  //           } else {
-  //             reject(error);
-  //           }
-  //         }
-  //       );
-  //       streamifier.createReadStream(stream).pipe(cloudinaryStream);
-  //     });
-  //   } catch (err) {
-  //     throw new AppError(
-  //       'There is a problem uploading your image! Please contact the system administrator.',
-  //       500
-  //     );
-  //   }
+  //   streamifier.createReadStream(stream).pipe(cloudinaryStream);
   // };
+
+  const cloudinaryUpload = async stream => {
+    try {
+      await new Promise((resolve, reject) => {
+        const cloudinaryStream = cloudinary.uploader.upload_stream(
+          {
+            upload_preset: 'kinetotickets-shows'
+          },
+          function(error, result) {
+            if (result) {
+              cloudinaryId = result.public_id;
+              cloudinaryUrl = result.secure_url;
+              resolve(cloudinaryId);
+            } else {
+              reject(error);
+            }
+          }
+        );
+        streamifier.createReadStream(stream).pipe(cloudinaryStream);
+      });
+    } catch (err) {
+      throw new AppError(
+        'There is a problem uploading your image! Please contact the system administrator.',
+        500
+      );
+    }
+  };
 
   console.log('CloudinaryID: ', cloudinaryId);
   console.log('CloudinaryURL: ', cloudinaryUrl);
@@ -97,4 +97,4 @@ exports.uploadPhoto = catchAsync(async (req, res, next) => {
   // };
 
   next();
-});
+};
