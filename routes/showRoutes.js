19c2751c5049ddd6719f146e3e805ba5ae91aebf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-23 20:59:08 -0700

    Refactored

--- routes/showRoutes.js
+++ routes/showRoutes.js
@@ -54,7 +54,7 @@ router.patch(
 router.patch(
   '/updateMyShow/:id/:showPoster',
   authController.restrictTo('event-owner'),
-  photoController.deletePhoto,
+  photoController.deletePhoto('kinetotickets/shows'),
   photoController.bufferPhoto('poster'),
   photoController.uploadPhoto('kinetotickets-shows'),
   showController.updateMyShow
