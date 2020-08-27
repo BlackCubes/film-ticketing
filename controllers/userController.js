692c2174c0283e5057d331f5647fabf70dfd4a61
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-26 21:34:54 -0700

    Refactored

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,75 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
-const multer = require('multer');
-const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
-
-// MULTER
-// const multerStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'public/img/users');
-//   },
-//   filename: (req, file, cb) => {
-//     const ext = file.mimetype.split('/')[1];
-//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-//   }
-// });
-
-const multerStorage = multer.memoryStorage();
-
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    cb(
-      new AppError('This is not an image! Please upload only images!', 400),
-      false
-    );
-  }
-};
-
-const upload = multer({
-  storage: multerStorage,
-  fileFilter: multerFilter
-});
-
-exports.uploadUserPhoto = upload.single('photo');
-
-exports.resizeUserPhoto = (req, res, next) => {
-  if (!req.file) return next();
-
-  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-
-  sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat('jpeg')
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
-
-  next();
-};
-
-exports.deletePhoto = catchAsync(async (req, res, next) => {
-  if (!req.params.userPhoto || req.params.userPhoto.split('.')[0] === 'default')
-    return next();
-  if (
-    req.params.userPhoto.split('.')[1] !== 'jpeg' ||
-    req.params.userPhoto.split('-').length !== 3 ||
-    req.params.userPhoto.split('-')[0] !== 'user'
-  )
-    return next(new AppError('This route is for updating photos!', 400));
-
-  const unlinkAsync = promisify(fs.unlink);
-  const photoPath = path.join('public/img/users/', req.params.userPhoto);
-
-  await unlinkAsync(photoPath);
-
-  next();
-});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
