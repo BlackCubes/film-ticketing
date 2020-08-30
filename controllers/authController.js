27dfd498dc0d152c007a0ff7a9ea036c23bdba37
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-08-29 18:14:33 -0700

    Debugging

--- controllers/authController.js
+++ controllers/authController.js
@@ -186,7 +186,10 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     )}/api/v1/users/resetPassword/${resetToken}`;
 
     const ip =
-      (req.headers['x-forwarded-for'] || '').split(',')[0].trim() ||
+      (req.headers['x-forwarded-for'] || '')
+        .split(',')
+        .pop()
+        .trim() ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       req.connection.socket.remoteAddress;
