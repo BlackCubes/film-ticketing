42e3850c23724f25818562aa58f9d185be96f296
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-05 16:32:44 -0700

    Refactored

--- routes/castcrewRoutes.js
+++ routes/castcrewRoutes.js
@@ -11,12 +11,14 @@ router.use('/:castcrewId/shows', showRouter);
 router.route('/').get(castcrewConroller.getAllCastCrew);
 router.route('/:id').get(castcrewConroller.getCastCrew);
 
-router.use(authController.protect);
-router.use(authController.restrictTo('admin'));
+// router.use(authController.protect);
+// router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
   .post(
+    authController.protect,
+    authController.restrictTo('admin'),
     photoController.bufferPhoto('photo'),
     photoController.uploadPhoto('kinetotickets-castcrews'),
     castcrewConroller.rolesParse,
@@ -25,11 +27,23 @@ router
 
 router
   .route('/:id')
-  .patch(castcrewConroller.rolesParse, castcrewConroller.updateCastCrew)
-  .delete(authController.verifyPassword, castcrewConroller.deleteCastCrew);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    castcrewConroller.rolesParse,
+    castcrewConroller.updateCastCrew
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    authController.verifyPassword,
+    castcrewConroller.deleteCastCrew
+  );
 
 router.patch(
   '/:id/:castcrewsPhoto',
+  authController.protect,
+  authController.restrictTo('admin'),
   photoController.deletePhoto('castcrews'),
   photoController.bufferPhoto('photo'),
   photoController.uploadPhoto('kinetotickets-castcrews'),
