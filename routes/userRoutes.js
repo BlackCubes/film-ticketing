df86a14fc1f78f1e45ad309b67c57992b35eaec7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-04 21:52:17 -0700

    Imported the validation middleware controller and implemented it to login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 const photoController = require('./../controllers/photoController');
+const validationController = require('./../controllers/validationController');
 const showRouter = require('./showRoutes');
 
 const router = express.Router({ mergeParams: true });
@@ -9,7 +10,7 @@ const router = express.Router({ mergeParams: true });
 router.use('/:userId/shows', showRouter);
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/login', validationController.login, authController.login);
 router.get('/logout', authController.logout);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
