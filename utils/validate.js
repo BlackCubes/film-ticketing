27faf139557ca733820a95c7043077987d3db6e8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-04 22:16:23 -0700

    Added a regex validation

--- utils/validate.js
+++ utils/validate.js
@@ -1,5 +1,14 @@
 const Validator = require('validatorjs');
 
+// const regexPass = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[.#?!@$%^&*\\-_]).{8,60}$/;
+const regexPass = /^(?=.*?[0-9])(?=.*?[a-z]).{8,60}$/;
+
+Validator.register(
+  'strict',
+  val => regexPass.test(val),
+  'Please use at least one number, one special character, and one capital letter between 8 to 60 characters'
+);
+
 module.exports = (body, rules, customMessages, cb) => {
   const validation = new Validator(body, rules, customMessages);
 
