f496ec4a6e821e9bc0f32a72903201083c81ad61
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-04 21:23:49 -0700

    Changed the function

--- utils/validate.js
+++ utils/validate.js
@@ -1,6 +1,6 @@
 const Validator = require('validatorjs');
 
-exports.validator = (body, rules, customMessages, cb) => {
+module.exports = (body, rules, customMessages, cb) => {
   const validation = new Validator(body, rules, customMessages);
 
   validation.passes(() => cb(null, true));
