151d3c74c85cdd444e3a764307734d642fbefb2e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-07 15:55:08 -0700

    Added the optional review rating regex validator

--- utils/validate.js
+++ utils/validate.js
@@ -29,6 +29,7 @@ const regexZipCodeOpt = /^([0-9]{5})?$/;
 const regexLongitude = /^[+-]?((1[0-7]|[1-9])?\d(\.\d+)?|180(\.0+)?)$/;
 const regexLatitude = /^[+-]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/;
 const regexRating = /^[1-5]{1}$/;
+const regexRatingOpt = /^([1-5]{1})?$/;
 
 Validator.register(
   'regexName',
@@ -190,6 +191,11 @@ Validator.register(
 Validator.register(
   'regexRating',
   val => regexRating.test(val),
+  'Please provide a valid rating between 1 and 5.'
+);
+Validator.register(
+  'regexRatingOpt',
+  val => regexRatingOpt.test(val),
   'Please provide a valid rating between 1 and 5.'
 );
 
