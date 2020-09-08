381b0eb8cc7df62df6bc52f90fb385efbcd24f96
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-07 18:15:47 -0700

    Exported all the Models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const CastCrew = require('./castcrewModel');
+const Review = require('./reviewModel');
+const Show = require('./showModel');
+const Showtimes = require('./showtimesModel');
+const Theater = require('./theaterModel');
+const User = require('./userModel');
+
+module.exports = {
+  CastCrew,
+  Review,
+  Show,
+  Showtimes,
+  Theater,
+  User
+};
