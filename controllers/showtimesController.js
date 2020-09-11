ed87ae156a96fd2852768bdfda28f9e6e75dbe16
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-11 06:59:13 -0700

    Refactored

--- controllers/showtimesController.js
+++ controllers/showtimesController.js
@@ -25,8 +25,6 @@ exports.checkSoldOut = catchAsync(async (req, res, next) => {
     theaters: req.params.theaterId
   });
 
-  console.log(showtimesSoldOut);
-
   if (showtimesSoldOut && showtimesSoldOut.soldOut)
     return next(new AppError('This show has been sold out.', 401));
 
