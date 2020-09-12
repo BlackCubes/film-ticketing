e6483506bb732c54c890eb0f4377dfe5e010166d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-12 08:27:47 -0700

    Refactored and added expired middleware into route

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -9,8 +9,9 @@ router.get(
   '/checkout-session/:showId/:theaterId/:showtimeId',
   authController.protect,
   authController.restrictTo('admin', 'user'),
-  showtimesController.checkSoldOut,
+  showtimesController.checkExpired,
   ticketController.checkTicketExists,
+  showtimesController.checkSoldOut,
   ticketController.getCheckoutSession
 );
 
