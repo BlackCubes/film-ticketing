4c54dce2b5a309a976896df81fda4933d20ab3f4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-20 10:30:19 -0700

    Refactored

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -27673,40 +27673,7 @@ var MultiForm = /*#__PURE__*/function () {
       // e.preventDefault();
       this.count++;
       this.formSlide(this.count);
-    } // buttonNext(e, input = false, select = false, textarea = false) {
-    //   e.preventDefault();
-    //   let multiFormFieldset = document.querySelectorAll('fieldset')[this.count],
-    //     multiFormVal = true;
-    //   let multiFormInputs, multiFormSelects, multiFormTextareas;
-    //   if (input === true) {
-    //     multiFormInputs = multiFormFieldset.querySelectorAll('input');
-    //     multiFormInputs.forEach(element => {
-    //       if (element.required)
-    //         multiFormVal = formValidator(element.value, 'input', multiFormVal);
-    //     });
-    //   }
-    //   if (select === true) {
-    //     multiFormSelects = multiFormFieldset.querySelectorAll('select');
-    //     multiFormSelects.forEach(element => {
-    //       if (element.required) {
-    //         let option = element.options[element.selectedIndex];
-    //         multiFormVal = formValidator(option.value, 'select', multiFormVal);
-    //       }
-    //     });
-    //   }
-    //   if (textarea === true) {
-    //     multiFormTextareas = multiFormFieldset.querySelectorAll('textarea');
-    //     multiFormTextareas.forEach(element => {
-    //       if (element.required)
-    //         multiFormVal = formValidator(element.value, 'textarea', multiFormVal);
-    //     });
-    //   }
-    //   if (multiFormVal === true) {
-    //     this.count++;
-    //     this.formSlide(this.count);
-    //   }
-    // }
-
+    }
   }]);
 
   return MultiForm;
