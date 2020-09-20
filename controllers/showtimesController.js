b73b39adb46621aa141be83d92648f072bd7b16c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-20 09:54:27 -0700

    Refactored

--- controllers/showtimesController.js
+++ controllers/showtimesController.js
@@ -18,7 +18,7 @@ exports.checkExpired = catchAsync(async (req, res, next) => {
     _id: req.params.showtimeId
   });
 
-  if (showtimesExpired && Date.now() > showtimesExpired.endDateTime)
+  if (showtimesExpired && Date.now() >= showtimesExpired.startDateTime)
     return next(new AppError('This show has been expired.', 403));
 
   next();
@@ -64,86 +64,8 @@ exports.createShowtime = factory.createOne(Showtimes);
 exports.updateShowtime = factory.updateOne(Showtimes);
 exports.deleteShowtime = factory.deleteOne(Showtimes);
 
-// exports.getAllShowtimes = catchAsync(async (req, res, next) => {
-//   const filter = {};
-//   if (req.params.showId) filter.shows = req.params.showId;
-//   if (req.params.theaterId) filter.theaters = req.params.theaterId;
-
-//   const showtimes = await Showtimes.find(filter);
-
-//   res.status(200).json({
-//     status: 'success',
-//     results: showtimes.length,
-//     data: {
-//       showtimes
-//     }
-//   });
-// });
-
-// exports.getShowtime = catchAsync(async (req, res, next) => {
-//   const showtime = await Showtimes.findById(req.params.id);
-
-//   if (!showtime) {
-//     return next(new AppError('There is no showtime with that id!', 404));
-//   }
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       showtime
-//     }
-//   });
-// });
-
-// exports.createShowtime = catchAsync(async (req, res, next) => {
-//   // LATER, CREATE CODE TO CHECK TO SEE IF DATA EXISTS, AND IF SO, THEN ERROR
-//   if (!req.body.shows) req.body.shows = [req.params.showId];
-//   if (!req.body.theaters) req.body.theaters = [req.params.theaterId];
-
-//   const newShowtime = await Showtimes.create(req.body);
-
-//   res.status(201).json({
-//     status: 'success',
-//     data: {
-//       showtime: newShowtime
-//     }
-//   });
-// });
-
-// exports.updateShowtime = catchAsync(async (req, res, next) => {
-//   const showtime = await Showtimes.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true
-//   });
-
-//   if (!showtime) {
-//     return next(new AppError('There is no showtime with that id!', 404));
-//   }
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       showtime
-//     }
-//   });
-// });
-
-// exports.deleteShowtime = catchAsync(async (req, res, next) => {
-//   const showtime = await Showtimes.findByIdAndDelete(req.params.id);
-
-//   if (!showtime) {
-//     return next(new AppError('There is no showtime with that id!', 404));
-//   }
-
-//   res.status(204).json({
-//     status: 'success',
-//     data: null
-//   });
-// });
-
 exports.getDailyPlan = catchAsync(async (req, res, next) => {
   const { date } = req.params;
-  console.log(date);
 
   const plan = await Showtimes.aggregate([
     {
