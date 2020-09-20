c998b4ee1a5ee162527310fa19ff7c1eb2e8ec90
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-20 10:22:32 -0700

    Refactored

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -26769,21 +26769,6 @@ var _alerts = require("./alerts");
 var _utils = require("./utils");
 
 /* eslint-disable */
-// export const formValidator = (input, type = '', bol) => {
-//   if (type === 'input' && !input) {
-//     showAlert('error', 'Error on input!');
-//     bol = false;
-//   }
-//   if (type === 'select' && !input) {
-//     showAlert('error', 'Error on the select!');
-//     bol = false;
-//   }
-//   if (type === 'textarea' && !input) {
-//     showAlert('error', 'Error on the textarea!');
-//     bol = false;
-//   }
-//   return bol;
-// };
 var attributeError = function attributeError(input, message) {
   if (!input) {
     (0, _alerts.showAlert)('error', message);
@@ -31628,7 +31613,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58652" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49211" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
