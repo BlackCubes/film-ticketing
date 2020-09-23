d26e9bb77395b34edd6d1455c97e85332f78d852
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-22 22:43:17 -0700

    Created middleware for non-parsed geo

--- controllers/theaterController.js
+++ controllers/theaterController.js
@@ -12,6 +12,13 @@ exports.geoParse = (req, res, next) => {
   next();
 };
 
+exports.geoNonParse = (req, res, next) => {
+  if (!req.body.geoLong || !req.body.geoLat) return next();
+
+  req.body.geo = { coordinates: [req.body.geoLong, req.body.geoLat] };
+  next();
+};
+
 exports.getAllTheaters = factory.getAll(Theater);
 exports.getTheater = factory.getOne(Theater, 'showtimes');
 exports.createTheater = factory.createOne(Theater);
