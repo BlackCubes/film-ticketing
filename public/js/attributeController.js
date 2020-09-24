2322e0816bd03c6a0f76647ec74e2cc45c799f3a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-09-23 18:19:48 -0700

    Debugging

--- public/js/attributeController.js
+++ public/js/attributeController.js
@@ -21,6 +21,11 @@ export const validateAttribute = (attribute, ...dataSets) => {
 
   if (attributeName) {
     if (!validateRegex('attribute-name', attributeName)) {
+      console.log(
+        'Validation regex: ',
+        !validateRegex('attribute-name', attributeName)
+      );
+      console.log('Attribute name: ', attributeName);
       attributeError(attribute, 'Invalid name');
       attributeStatus = false;
     }
