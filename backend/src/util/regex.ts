66199ecd5610071c2512aa840b01db40449bb158
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-07-11 14:41:16 -0700

    feat(regex): add role and gender regex

--- backend/src/util/regex.ts
+++ backend/src/util/regex.ts
@@ -1,3 +1,5 @@
+import { TUserGender, TUserRole } from '@src/types/user.types';
+
 const imageFileTypes = /jpeg|jpg|png|webp/;
 
 const mailingAddress = /^(\d{1,}) [a-zA-Z0-9\s]+(\,)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}$/;
@@ -6,4 +8,8 @@ const phoneNumber = /^\d{10}$/;
 
 const uniqueCode = /^[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[\]:;.,<>/?]{10}$/;
 
-export { imageFileTypes, mailingAddress, phoneNumber, uniqueCode };
+const userRole: TUserRole[] = ['admin', 'event-owner', 'user'];
+
+const userGender: TUserGender[] = ['f', 'm', 'p'];
+
+export { imageFileTypes, mailingAddress, phoneNumber, uniqueCode, userGender, userRole };
