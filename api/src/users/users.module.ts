4a049ed94c39bad15c9d5dc94fdb7c367cf10d35
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-09-09 15:36:23 -0700

    feat(users-module): import mongodb

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,8 +1,48 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+
+          schema.index({ email: 'text', username: 'text' });
+
+          schema.pre('save', async function (next) {
+            if (!this.isModified('password') || !this.password) {
+              return next();
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(this.password, salt);
+
+            this.password = hash;
+
+            return next();
+          });
+
+          schema.pre('save', async function (next) {
+            if (!this.isModified('password') || this.isNew) {
+              return next();
+            }
+
+            this.passwordChangedAt = new Date(Date.now() - 1000);
+
+            next();
+          });
+
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
