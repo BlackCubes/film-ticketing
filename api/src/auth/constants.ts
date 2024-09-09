4b9a386ec57cfd202d59ee87ca729a2c7558196c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-09-09 16:20:37 -0700

    feat(auth-constants): add jwt constants

---
+++ api/src/auth/constants.ts
@@ -0,0 +1,6 @@
+export const jwtConstants = {
+  // SECURITY WARNING: DO NOT USE DURING PRODUCTION!
+  expires: '1d',
+  // SECURITY WARNING: DO NOT USE DURING PRODUCTION!
+  secret: 'nestjs-insecure-tninjp$*k_fa!*f2-(na_vv)slx!^hf-*7r63ccwsi^xk2u)1+',
+};
