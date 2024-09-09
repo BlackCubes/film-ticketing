ea33bf1ebd26efdff65ab263accbbcab55c50444
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-09-09 10:50:26 -0700

    feat(app-module): wire up database

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.DATABASE?.replace(
+        '<password>',
+        process.env.DATABASE_PASSWORD ?? '',
+      ) ?? '',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
