d2f44ed2b17664c0b5b7ed97399fc1f2bdfb2b25
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-09-09 12:56:39 -0700

    feat(main): add securities

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: process.env?.NODE_ENV === 'production' ? true : false,
+    rawBody: true,
+  });
+
+  app.use(helmet());
+
+  app.enableCors();
+
+  app.useBodyParser('json', { limit: '10mb' });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
