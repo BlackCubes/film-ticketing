f9aaa8bb59f86c0fb1a23609acff03cd5a25acc2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-09-09 12:40:53 -0700

    feat(app-module): config environment variables

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +7,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       process.env.DATABASE?.replace(
         '<password>',
